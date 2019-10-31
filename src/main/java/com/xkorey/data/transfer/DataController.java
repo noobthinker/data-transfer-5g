704d7363db707ed42936c19967371364e218d120
xkorey <xkorey@gmail.com>
2019-11-01 00:25:22 +0800

    init project

---
+++ src/main/java/com/xkorey/data/transfer/DataController.java
@@ -0,0 +1,25 @@
+package com.xkorey.data.transfer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class DataController {
+
+  @RequestMapping({"/", "/index", "/index.html"})
+  public Map dataReceiver(HttpServletRequest request) {
+    return request.getParameterMap();
+  }
+
+  @RequestMapping("/{str}")
+  public String stringAll(@PathVariable("str") String str) {
+    log.info("txt {}", str);
+    return str;
+  }
+}
